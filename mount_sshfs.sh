55fbd414145b5b003292222107ab0bc1c6343e72
adribr53 <adrien.giot@student.uclouvain.be>
2023-02-12 15:17:16 +0100

    Progress done

--- mount_sshfs.sh
+++ mount_sshfs.sh
@@ -1,9 +1,6 @@
 arg1=$1
 arg2=$2
 echo $2
-ssh $1.g5k "mkdir -p $2"
-cp setup_machine.sh ~/mount-$3
-scp -r ~/mount-$3/* $1.g5k:/home/agiot/$2
-ssh $1.g5k "mkdir -p $2"
-ssh $1.g5k "ssh root@$2 "mkdir data""
-ssh $1.g5k "sshfs -o idmap=user root@$2:/data /home/agiot/$2"
+cp setup_machine.sh ~/mount-g5k
+scp -r ~/mount-g5k/* $1.g5k:/home/agiot/$2
+ssh $1.g5k "sshfs -o idmap=user root@$2:/data /home/agiot"
