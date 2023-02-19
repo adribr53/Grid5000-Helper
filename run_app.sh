f48c5030c67f3aa3448506fdab1233ce6b99bae8
Maxime de Neuville <56126588+deneuvillem@users.noreply.github.com>
2023-02-19 23:31:35 +0100

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -2,13 +2,15 @@
 # Execute deploy_lwip_dpdk script
 
 # PARAMETERS :
-# First parameter is the listening port number
+# First parameter is the content length of the HTTP response
+# Second parameter is the listening port number
 
 cd 
 cd tinyhttpd-lwip-dpdk/
 make 
 
-port_number=$1
+content_len=$1
+port_number=$2
 interfacenum="$(cat myinterfacenum)"
 addr="$(cat myaddr)"
 gw="$(cat mygw)"
@@ -20,4 +22,4 @@ echo $addr
 echo $gw
 echo $mask
 
-LD_LIBRARY_PATH=./dpdk/install/lib/x86_64-linux-gnu ./app -l 0-1 --proc-type=primary --file-prefix=pmd1 --allow=$interfacenumber -- -a $addr -g $gw -m $mask -l 1 -p $port_number
+LD_LIBRARY_PATH=./dpdk/install/lib/x86_64-linux-gnu ./app -l 0-1 --proc-type=primary --file-prefix=pmd1 --allow=$interfacenumber -- -a $addr -g $gw -m $mask -l $content_len -p $port_number
